86e112ea930142a2cbc1c2c23133d5874e8833aa
Artyom Nazaroff <artyom.nazaroff@yandex.ru>
2024-01-16 19:29:47 +0600

    Create board

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
+import { BoardComponent } from './components/BoardComponent';
+import { Board } from './models/Board';
 
 const App = () => {
-  return (
-    <div>
+	const [board, setBoard] = useState(new Board())
 
-    </div>
-  );
+	useEffect(() => {
+		restart()
+	}, [])
+
+	const restart = () => {
+		const newBoard = new Board()
+		newBoard.initCells()
+		setBoard(newBoard)
+	}
+
+	return (
+		<div className='app'>
+			<BoardComponent
+				board={board}
+				setBoard={setBoard}
+			/>
+		</div>
+	);
 };
 
 export default App;
